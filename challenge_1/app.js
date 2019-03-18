8fc11a9970a970ebd0f8c5a94c5ba56ae2eb865d
Theresa <tli2@cca.edu>
2019-03-18 16:32:57 -0700

    Reformat and lay out app in MVC format

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,12 +1,42 @@
-var currentPlayer = 'x'; // Player 'x' goes first
-
-var placeMarker = (location) => {
-  document.getElementById(location)
-    .innerHTML = '<p style="text-align: center; font-size: 25px">' + currentPlayer + '</p>';
-    
-  if (currentPlayer === 'x') {
-    currentPlayer = 'o';
-  } else {
-    currentPlayer = 'x';
+// Model
+class Board {
+  constructor() {
+    this.state = {};
   }
-};
+
+  init() {
+    this.state = {
+      currentPlayer: 'x',
+      currentBoard: [
+        [null, null, null], 
+        [null, null, null], 
+        [null, null, null]
+      ]
+    }
+  }
+
+  // Mark selected square in state
+  mark() {
+
+  }
+
+  // Check for match
+  checkStatus() {
+
+  }
+}
+
+// View
+// Update DOM with current board
+
+
+// Controller
+window.onload = () => {
+  var board = new Board();
+  board.init();
+  // update with view
+}
+
+//On box click
+  // Mark on board
+  // Update view on corresponding location
