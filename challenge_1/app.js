01beb537b3f5d35c93ce550a580c221c69ab5f91
Theresa <tli2@cca.edu>
2019-03-18 12:33:41 -0700

    Implement placeMarker function

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,12 @@
+var currentPlayer = 'x'; // Player 'x' goes first
 
-
+var placeMarker = (location) => {
+  document.getElementById(location)
+    .innerHTML = '<p style="text-align: center; font-size: 25px">' + currentPlayer + '</p>';
+    
+  if (currentPlayer === 'x') {
+    currentPlayer = 'o';
+  } else {
+    currentPlayer = 'x';
+  }
+};
