1a7b115e7b62390177a7d88be1e4a44dd338ce53
Theresa <tli2@cca.edu>
2019-03-19 11:14:46 -0700

    Implement partial checks for wins

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -20,23 +20,76 @@ class Board {
       document.getElementById('messages').innerHTML = 'That spot is already taken!';
       return;
     }
-    document.getElementById('messages').innerHTML = '';
+    document.getElementById('messages').innerHTML = `It's player ${this.state.currentPlayer}'s turn!`;
     this.state.currentBoard[row][column] = this.state.currentPlayer;
+    this.checkStatus(row, column);
     if (this.state.currentPlayer === 'x') {
       this.state.currentPlayer = 'o';
     } else {
       this.state.currentPlayer = 'x';
-    }
+    }  
   }
 
   // Check for match
-  checkStatus() {
+  checkStatus(row, column) {
+    console.log('Checkity!');
+    this.checkRow(row);
+    this.checkCol(column);
+    this.checkMajDiag(row,column);
+    this.checkMinDiag(row,column);
+  }
+
+  winMessage() {
+    document.getElementById('messages').innerHTML = `Player ${this.state.currentPlayer} has won!`;
+  };
+  checkRow(row) {
+    var row = this.state.currentBoard[row];
+    for (var c = 0; c < row.length; c++) {
+      if (row[c] !== this.state.currentPlayer) {       
+        return; 
+      }
+      winMessage();
+    }
+  }
+  checkCol(column) {
+    var board = this.state.currentBoard;
+    for (var r = 0; r < board.length; r++) {
+      if (board[r][column] !== this.state.currentPlayer) {
+        return;
+      }
+      winMessage();
+    }
+  }
+  checkMajDiag(row, column) { // Top left to bottom right
+    var startingRow;
+    var startingColumn;
+    var board = this.state.currentBoard;
+
+    if (row < column) {
+      startingRow = 0;
+      startingColumn = column - row;
+    } else {
+      startingRow = row - column;
+      startingColumn = 0;
+    }
+
+    for (var r = startingRow; r < board.length; i++) {
+      if (board[r][startingColumn] !== this.state.currentPlayer) {
+          return;
+      }
+      startingColumn++;
+    }
+
+    winMessage();
+  }
+  checkMinDiag(row, column) { // Bottom left to top right
 
   }
 }
 
 // VIEW
 var updateView = () => {
+  document.getElementById('messages').innerHTML = `It's player ${board.state.currentPlayer}'s turn!`;
   var boxArr = document.getElementById('board').children;
   for (var i = 0; i < boxArr.length; i++) {
     var row = boxArr[i].children;
