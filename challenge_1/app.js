7d6b16716596abd395acb361a447874d79e3381a
Theresa <tli2@cca.edu>
2019-03-18 17:41:16 -0700

    Implement event listeners and board mark function

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -15,9 +15,14 @@ class Board {
     }
   }
 
-  // Mark selected square in state
-  mark() {
-
+  mark(row, column) {
+    this.state.currentBoard[row][column] = this.state.currentPlayer;
+    if (this.state.currentPlayer === 'x') {
+      this.state.currentPlayer = 'o';
+    } else {
+      this.state.currentPlayer = 'x';
+    }
+    console.log(this.state);
   }
 
   // Check for match
@@ -31,12 +36,28 @@ class Board {
 
 
 // Controller
+var board;
 window.onload = () => {
-  var board = new Board();
+  board = new Board();
   board.init();
   // update with view
 }
 
-//On box click
-  // Mark on board
-  // Update view on corresponding location
+var boxes = document.getElementsByClassName('box');
+Array.from(boxes).forEach((box) => {
+  box.addEventListener('click', (e) => {
+    var location = e.srcElement.id.split(/row|column/);
+    board.mark(location[1] - 1, location[2] - 1);
+  });
+});
+
+
+
+
+
+// var placeMarker = (location) => {
+//   document.getElementById(location)
+//     .innerHTML = '<p style="text-align: center; font-size: 25px">' + currentPlayer + '</p>';
+
+
+// };
