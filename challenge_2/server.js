342e076f8358aa9c1eea917381fc94668ee00b2e
Theresa <tli2@cca.edu>
2019-03-19 17:30:07 -0700

    Connect client and server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,14 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('bodyParser');
+var port = 8000;
 
+app.use(express.static('client'));
+app.use(bodyParser.json());
 
+app.post('/', (req, res) => {
+  console.log('req', req.body);
+  res.send('hi!');
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
