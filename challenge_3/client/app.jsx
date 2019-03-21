993d021e388643ba6037fb093eb25ed06e91f718
Theresa <tli2@cca.edu>
2019-03-20 17:16:51 -0700

    Connect client to server and implement code for Checkout and Signup page

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,44 @@
+class Checkout extends React.Component {
+  constructor () {
+    super()
+  }
 
+  render () {
+    return (
+      <button onClick={this.checkoutClickHandler}>Checkout</button>
+    );
+  }
+  
+  checkoutClickHandler() {
+    ReactDOM.render(<Signup />, document.getElementById('app'));
+  }
+}
 
+class Signup extends React.Component {
+  constructor () {
+    super()
+  }
+
+  render () {
+    return (
+      <form action="/signup" method="POST" className="form-signup">
+        <h1>Sign Up</h1>
+        <div>
+          <label htmlFor="name">Name: </label><br />
+          <input id="name" type="text"></input>
+        </div>
+        <div>
+          <label htmlFor="email">Email: </label><br />
+          <input id="email" type="email"></input>
+        </div>
+        <div>
+          <label htmlFor="password">Password: </label><br />
+          <input id="password" type="password"></input>
+        </div>
+        <input type="submit" value="Next"></input>
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(<Checkout />, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,14 @@
+var express = require('express');
+var app = express();
+
+var port = 8000;
+
+app.post('/signup', (req, res) => {
+  res.send();
+});
+
+app.use(express.static('public'));
+
+app.listen(port, console.log(`Server running at ${port}`));
 
 
