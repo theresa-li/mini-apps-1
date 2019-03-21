5e625959186444773fa2ac4f999aa7fad683d8b0
Theresa <tli2@cca.edu>
2019-03-20 17:37:05 -0700

    Implement code for Shipping page

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -35,10 +35,56 @@ class Signup extends React.Component {
           <label htmlFor="password">Password: </label><br />
           <input id="password" type="password"></input>
         </div>
-        <input type="submit" value="Next"></input>
+        <input type="submit" value="Next" onClick={this.signupClickHandler}></input>
       </form>
     );
   }
+
+  signupClickHandler () {
+    ReactDOM.render(<Shipping />, document.getElementById('app'));
+  }
+}
+
+class Shipping extends React.Component {
+  constructor () {
+    super()
+  }
+
+  render () {
+    return (
+      <form action="/shipping" method="POST" className="form-shipping">
+        <h1>Address: </h1>
+        <div>
+          <label htmlFor="lineOne">Line 1: </label><br />
+          <input id="lineOne" type="text"></input>
+        </div>
+        <div>
+          <label htmlFor="lineTwo">Line 2: </label><br />
+          <input id="lineTwo" type="text"></input>
+        </div>
+        <div>
+          <label htmlFor="city">City: </label><br />
+          <input id="city" type="text"></input>
+        </div>
+        <div>
+          <label htmlFor="state">State: </label><br />
+          <input id="state" type="text"></input>
+        </div>
+        <div>
+          <label htmlFor="zipcode">Zipcode: </label><br />
+          <input id="zipcode" type="text"></input>
+        </div>
+        <input type="submit" value="Next" onClick={this.shippingClickHandler}></input>
+      </form>
+    );
+  }
+
+  shippingClickHandler () {
+    ReactDOM.render(<Payment />, document.getElementById('app'));
+  }
+}
+
+class Payment extends React.Component {
 }
 
 ReactDOM.render(<Checkout />, document.getElementById('app'));
