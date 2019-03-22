717244472a19fe35ec563ed314b735099e7ac878
Theresa <tli2@cca.edu>
2019-03-21 19:49:02 -0700

    Connect server to client

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,7 @@
+var express = require('express');
+var app = express();
+var port = 8080;
 
+app.use(express.static('client/dist'));
 
+app.listen(port, () => console.log(`Listening on port ${port}`));
